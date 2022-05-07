99154bb1703cdef656ab9a354e2ec47881c450a2
Kent Cederström <kent@kentcederstrom.se>
2022-05-07 20:18:52 +0200

    [app] hook up feature toggle to api

--- app/features/toggles/FeatureToggle.tsx
+++ app/features/toggles/FeatureToggle.tsx
@@ -1,15 +1,19 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { ReactNode } from 'react';
+import { useQuery } from 'react-query';
+import { get } from '../../libs/api';
 
 interface FeatureToggle {
   artists: boolean;
   schedule: boolean;
   deals: boolean;
+  news: boolean;
 }
 
 const defaultState = {
   artists: true,
   schedule: true,
   deals: true,
+  news: true,
 };
 
 export const FeatureToggleContext = React.createContext<FeatureToggle>(defaultState);
@@ -19,19 +23,16 @@ interface FeatureToggleProviderProps {
 }
 
 function FeatureToggleProvider({ children }: FeatureToggleProviderProps) {
-  const [features, setFeatures] = useState<FeatureToggle>();
-  useEffect(() => {
-    setTimeout(() => {
-      setFeatures({
-        artists: false,
-        schedule: false,
-        deals: true,
-      });
-    }, 15);
-  }, []);
+  const { data, status } = useQuery<FeatureToggle>({
+    queryKey: '/features',
+    queryFn: async ({ queryKey }) => {
+      const result = await get<FeatureToggle>(queryKey);
+      return result.data;
+    },
+  });
   return (
-    <FeatureToggleContext.Provider value={features ?? defaultState}>
-      {features && children}
+    <FeatureToggleContext.Provider value={data || defaultState}>
+      {status === 'success' && children}
     </FeatureToggleContext.Provider>
   );
 }
