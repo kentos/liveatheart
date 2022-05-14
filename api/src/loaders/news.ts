bdfde9cb50005d84894654d2390a08c571fcaa9d
Kent Cederström <kent@kentcederstrom.se>
2022-05-14 21:31:40 +0200

    try catch news fetcher

--- api/src/loaders/news.ts
+++ api/src/loaders/news.ts
@@ -5,16 +5,20 @@ import fs from 'fs'
 import path from 'path'
 
 export async function loadNews() {
-  const result = await axios.get('https://liveatheart.se/news22');
-  const html = parse(result.data);
-  const data = html.querySelectorAll('article.category-news22').map(a => {
-    return {
-      id: ulid(),
-      title: a.querySelector('h2')?.innerText,
-      link: a.querySelector('a')?.getAttribute('href'),
-      image: a.querySelector('img')?.getAttribute('src'),
-      published: a.querySelector('span.published')?.innerText,
-    }
-  })
-  fs.writeFileSync(path.join(__dirname, '..', 'data', 'news.json'), JSON.stringify(data, null, 2))
+  try {
+    const result = await axios.get('https://liveatheart.se/news22/');
+    const html = parse(result.data);
+    const data = html.querySelectorAll('article.category-news22').map(a => {
+      return {
+        id: ulid(),
+        title: a.querySelector('h2')?.innerText,
+        link: a.querySelector('a')?.getAttribute('href'),
+        image: a.querySelector('img')?.getAttribute('src'),
+        published: a.querySelector('span.published')?.innerText,
+      }
+    })
+    fs.writeFileSync(path.join(__dirname, '..', 'data', 'news.json'), JSON.stringify(data, null, 2))
+  } catch (e) {
+    console.log(e)
+  }
 }
