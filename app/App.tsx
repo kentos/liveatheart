2d3915d5518890cac52f99d8401dc18c891f884c
Kent Cederström <kent@kentcederstrom.se>
2022-05-15 08:45:58 +0200

    add sentry

--- app/App.tsx
+++ app/App.tsx
@@ -3,6 +3,13 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import FeatureToggleProvider from './features/toggles/FeatureToggle';
 import useCachedResources from './hooks/useCachedResources';
 import Navigation from './navigation';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://b071a2fc4d714e0586a76958d9aec17a@o1247194.ingest.sentry.io/6407070',
+  enableInExpoDevelopment: false,
+  debug: true,
+});
 
 const queryClient = new QueryClient();
 
