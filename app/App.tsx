9b5b6c73d8d60cc1e2ec1af88214de4ce529fd6b
Kent Cederström <kent@kentcederstrom.se>
2022-06-23 21:35:51 +0200

    [app] set react-query cacheTime to make android happy

--- app/App.tsx
+++ app/App.tsx
@@ -12,7 +12,13 @@ import Navigation from './navigation';
 //   debug: true,
 // });
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 1,
+    },
+  },
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
