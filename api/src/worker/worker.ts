a3d961895e9323d4da40b387ffc051ed564c617d
Kent Cederström <kent@kentcederstrom.se>
2022-07-09 20:44:30 +0200

    [api] handle data loaders better

--- api/src/worker/worker.ts
+++ api/src/worker/worker.ts
@@ -2,7 +2,18 @@ import schedule from 'node-schedule'
 import { loadArtists } from '../loaders/artists'
 import { loadNews } from '../loaders/news'
 
-schedule.scheduleJob('fetch news', '*/5 * * * *', loadNews)
-schedule.scheduleJob('fetch artists', '*/10 * * * *', loadArtists)
+function task(fn: () => Promise<void>) {
+  return async (fireDate: Date) => {
+    try {
+      console.log('[task]', fn.name, fireDate)
+      await fn()
+    } catch (e: any) {
+      console.log('[task error]:', e)
+    }
+  }
+}
+
+schedule.scheduleJob('fetch news', '*/5 * * * *', task(loadNews))
+schedule.scheduleJob('fetch artists', '*/10 * * * *', task(loadArtists))
 
 console.log(Object.keys(schedule.scheduledJobs))
