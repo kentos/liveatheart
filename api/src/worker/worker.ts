f7d9d991a4f22783fa28e210c7a4caa7f32c4156
Kent Cederström <kent@kentcederstrom.se>
2022-07-17 21:41:00 +0200

    [api] make data loaders only run in production

--- api/src/worker/worker.ts
+++ api/src/worker/worker.ts
@@ -1,3 +1,4 @@
+import getenv from 'getenv'
 import schedule from 'node-schedule'
 import { loadArtists } from '../loaders/artists'
 import { loadDeals } from '../loaders/deals'
@@ -14,8 +15,10 @@ function task(fn: () => Promise<void>) {
   }
 }
 
-schedule.scheduleJob('fetch news', '*/3 * * * *', task(loadNews))
-schedule.scheduleJob('fetch artists', '*/7 * * * *', task(loadArtists))
-schedule.scheduleJob('fetch deals', '*/13 * * * *', task(loadDeals))
+if (getenv('NODE_ENV', 'development') === 'production') {
+  schedule.scheduleJob('fetch news', '*/3 * * * *', task(loadNews))
+  schedule.scheduleJob('fetch artists', '*/7 * * * *', task(loadArtists))
+  schedule.scheduleJob('fetch deals', '*/13 * * * *', task(loadDeals))
+}
 
 console.log(Object.keys(schedule.scheduledJobs))
