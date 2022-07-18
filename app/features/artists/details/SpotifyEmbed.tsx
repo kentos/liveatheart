18c4a9feb0a00d9161aab40fff9d5c78613bd4c7
Kent Cederström <kent@kentcederstrom.se>
2022-07-18 18:00:22 +0200

    [app] hack to make webview embeds work on android

--- app/features/artists/details/SpotifyEmbed.tsx
+++ app/features/artists/details/SpotifyEmbed.tsx
@@ -9,7 +9,7 @@ function SpotifyEmbed({ uri }: Props) {
   return (
     <WebView
       source={{ uri }}
-      style={{ flex: 0, height: Layout.window.width / 1.78 }}
+      style={{ flex: 0, height: Layout.window.width / 1.78, opacity: 0.99 }}
       containerStyle={{ flex: 0 }}
     />
   );
--- app/features/artists/details/YoutubeEmbed.tsx
+++ app/features/artists/details/YoutubeEmbed.tsx
@@ -9,7 +9,7 @@ function YoutubeEmbed({ uri }: Props) {
   return (
     <WebView
       source={{ uri }}
-      style={{ flex: 0, height: Layout.window.width / 1.78 }}
+      style={{ flex: 0, height: Layout.window.width / 1.78, opacity: 0.99 }}
       containerStyle={{ flex: 0 }}
     />
   );
