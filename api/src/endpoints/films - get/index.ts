6806659bd85620124f3027a5337eba84de418c90
Kent Cederström <kent@kentcederstrom.se>
2023-06-27 22:25:56 +0200

    (api) cleanup

--- api/src/endpoints/films - get/index.ts
+++ api/src/endpoints/films - get/index.ts
@@ -1,39 +1,39 @@
-import { FastifyInstance } from '@heja/shared/fastify'
-import { collection } from '@heja/shared/mongodb'
-import { Film } from '../../features/types'
-import { LAHEvent } from '../../features/artists/types'
+// import { FastifyInstance } from '@heja/shared/fastify'
+// import { collection } from '@heja/shared/mongodb'
+// import { Film } from '../../features/types'
+// import { LAHEvent } from '../../features/artists/types'
 
-const dayMap: Record<string, string> = {
-  '31/08/2022': 'wednesday',
-  '01/09/2022': 'thursday',
-  '02/09/2022': 'friday',
-  '03/09/2022': 'saturday',
-}
+// const dayMap: Record<string, string> = {
+//   '31/08/2022': 'wednesday',
+//   '01/09/2022': 'thursday',
+//   '02/09/2022': 'friday',
+//   '03/09/2022': 'saturday',
+// }
 
-async function handler(fastify: FastifyInstance) {
-  fastify.route({
-    method: 'GET',
-    url: '/films',
-    handler: async () => {
-      const [films, events] = await Promise.all([
-        collection<Film>('films')
-          .find({ deletedAt: { $exists: false } })
-          .toArray(),
-        collection<LAHEvent>('events')
-          .find({ filmid: { $exists: true }, deletedAt: { $exists: false } })
-          .toArray(),
-      ])
-      return films.map((a) => ({
-        ...a,
-        slots: events
-          .filter((e) => e.filmid!.equals(a._id))
-          .map((e) => ({
-            ...e,
-            day: dayMap[e.date],
-          })),
-      }))
-    },
-  })
-}
+// async function handler(fastify: FastifyInstance) {
+//   fastify.route({
+//     method: 'GET',
+//     url: '/films',
+//     handler: async () => {
+//       const [films, events] = await Promise.all([
+//         collection<Film>('films')
+//           .find({ deletedAt: { $exists: false } })
+//           .toArray(),
+//         collection<LAHEvent>('events')
+//           .find({ filmid: { $exists: true }, deletedAt: { $exists: false } })
+//           .toArray(),
+//       ])
+//       return films.map((a) => ({
+//         ...a,
+//         slots: events
+//           .filter((e) => e.filmid!.equals(a._id))
+//           .map((e) => ({
+//             ...e,
+//             day: dayMap[e.date],
+//           })),
+//       }))
+//     },
+//   })
+// }
 
-export default handler
+// export default handler
--- api/src/endpoints/heartbeat artists - get/index.ts
+++ api/src/endpoints/heartbeat artists - get/index.ts
@@ -1,46 +1,46 @@
-import { FastifyInstance } from '@heja/shared/fastify'
-import authenticatedEndpoint from '../../lib/authenticateEndpoint'
-import onlyAdmins from '../../lib/onlyAdmins'
-import { ObjectId, collection } from '@heja/shared/mongodb'
-import { Artist } from '../../features/artists/types'
-import { User } from '../../features/users/types'
+// import { FastifyInstance } from '@heja/shared/fastify'
+// import authenticatedEndpoint from '../../lib/authenticateEndpoint'
+// import onlyAdmins from '../../lib/onlyAdmins'
+// import { ObjectId, collection } from '@heja/shared/mongodb'
+// import { Artist } from '../../features/artists/types'
+// import { User } from '../../features/users/types'
 
-type HeartedArtist = { _id: ObjectId; count: number; artist: Artist }
+// type HeartedArtist = { _id: ObjectId; count: number; artist: Artist }
 
-async function mostHeartedArtists() {
-  const result = await collection<User>('users')
-    .aggregate<HeartedArtist[]>([
-      { $match: { 'favorites.0': { $exists: true } } },
-      { $unwind: '$favorites' },
-      {
-        $group: { _id: { $toObjectId: '$favorites._id' }, count: { $sum: 1 } },
-      },
-      { $sort: { count: -1 } },
-      { $limit: 5 },
-      {
-        $lookup: {
-          from: 'artists',
-          localField: '_id',
-          foreignField: '_id',
-          as: 'artist',
-        },
-      },
-      { $unwind: '$artist' },
-    ])
-    .toArray()
-  return result
-}
+// async function mostHeartedArtists() {
+//   const result = await collection<User>('users')
+//     .aggregate<HeartedArtist[]>([
+//       { $match: { 'favorites.0': { $exists: true } } },
+//       { $unwind: '$favorites' },
+//       {
+//         $group: { _id: { $toObjectId: '$favorites._id' }, count: { $sum: 1 } },
+//       },
+//       { $sort: { count: -1 } },
+//       { $limit: 5 },
+//       {
+//         $lookup: {
+//           from: 'artists',
+//           localField: '_id',
+//           foreignField: '_id',
+//           as: 'artist',
+//         },
+//       },
+//       { $unwind: '$artist' },
+//     ])
+//     .toArray()
+//   return result
+// }
 
-async function handler(fastify: FastifyInstance) {
-  fastify.route({
-    method: 'GET',
-    url: '/heartbeat/artists',
-    preHandler: [authenticatedEndpoint, onlyAdmins],
-    handler: async () => {
-      const mostHearted = await mostHeartedArtists()
-      return mostHearted
-    },
-  })
-}
+// async function handler(fastify: FastifyInstance) {
+//   fastify.route({
+//     method: 'GET',
+//     url: '/heartbeat/artists',
+//     preHandler: [authenticatedEndpoint, onlyAdmins],
+//     handler: async () => {
+//       const mostHearted = await mostHeartedArtists()
+//       return mostHearted
+//     },
+//   })
+// }
 
-export default handler
+// export default handler
