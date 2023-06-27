2ce3756ea68e738a87ddc56ae01ecb612b5ce395
Kent Cederström <kent@kentcederstrom.se>
2023-06-27 20:34:49 +0200

    (app) move around code

--- app/features/more/More.tsx
+++ app/features/more/More.tsx
@@ -5,33 +5,11 @@ import { Body } from '../../components/Texts';
 import Colors from '../../constants/Colors';
 import { resetClient } from '../../contexts/session/restoreUserSession';
 
-function Item({ title, onPress }: { title: string; onPress?: () => void }) {
-  return (
-    <TouchableOpacity onPress={onPress}>
-      <View style={stylesItem.item}>
-        <Body color={Colors.light.tint}>{title}</Body>
-        <FontAwesome size={20} name="angle-right" color={Colors.light.tint} />
-      </View>
-    </TouchableOpacity>
-  );
-}
-
-const stylesItem = StyleSheet.create({
-  item: {
-    padding: 16,
-    backgroundColor: 'white',
-    borderBottomColor: Colors.light.border,
-    borderBottomWidth: 1,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-  },
-});
-
-function More() {
+export default function More() {
   const navigation = useNavigation();
   return (
     <ScrollView>
-      <Item title="Your profile" onPress={() => navigation.navigate('Profile')} />
+      {__DEV__ && <Item title="Your profile" onPress={() => navigation.navigate('Profile')} />}
       <Item title="Buy tickets" onPress={() => navigation.navigate('Tickets')} />
       <Item
         title="About Live at Heart"
@@ -64,4 +42,24 @@ function More() {
   );
 }
 
-export default More;
+function Item({ title, onPress }: { title: string; onPress?: () => void }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={stylesItem.item}>
+        <Body color={Colors.light.tint}>{title}</Body>
+        <FontAwesome size={20} name="angle-right" color={Colors.light.tint} />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const stylesItem = StyleSheet.create({
+  item: {
+    padding: 16,
+    backgroundColor: 'white',
+    borderBottomColor: Colors.light.border,
+    borderBottomWidth: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+});
