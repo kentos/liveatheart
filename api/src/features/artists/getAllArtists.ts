09af7bf7f9be097c9ce827bf9fa58a2d20dfe178
Kent Cederström <kent@kentcederstrom.se>
2023-06-30 13:49:21 +0200

    (api) try local instance in-memory cache

--- api/src/features/artists/getAllArtists.ts
+++ api/src/features/artists/getAllArtists.ts
@@ -1,8 +1,15 @@
 import { collection } from '@heja/shared/mongodb'
 import { Artist } from './types'
 
+let localCache: Artist[] = []
+const lastSet = Date.now()
+
 export async function getAllArtists() {
-  return collection<Artist>('artists')
-    .find({ deletedAt: { $exists: false } })
-    .toArray()
+  if (lastSet - Date.now() < 1000 * 60 * 2) {
+    // 2 minutes
+    localCache = await collection<Artist>('artists')
+      .find({ deletedAt: { $exists: false } })
+      .toArray()
+  }
+  return localCache
 }
