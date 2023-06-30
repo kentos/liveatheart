73d409ce8421bf56cf3ef401e084de7a4f650cd8
Kent Cederström <kent@kentcederstrom.se>
2023-06-30 13:53:43 +0200

    (api) check time

--- api/src/features/artists/getAllArtists.ts
+++ api/src/features/artists/getAllArtists.ts
@@ -5,7 +5,7 @@ let localCache: Artist[] = []
 const lastSet = Date.now()
 
 export async function getAllArtists() {
-  if (Date.now() - lastSet < 1000 * 60 * 2) {
+  if (Date.now() - lastSet > 1000 * 60 * 2) {
     // 2 minutes in-memory cache
     return localCache
   }
